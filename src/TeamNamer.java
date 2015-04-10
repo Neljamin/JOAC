ade089eb4abd7a88f4775686759a11ced136a21b
OysterKing <comhghalldavid@live.com>
2015-04-10 14:03:19 +0100

    Implemented basic name generation. Needs linguistic rules.

--- src/TeamNamer.java
+++ src/TeamNamer.java
@@ -1,15 +1,68 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Vector;
+
 
 public class TeamNamer {
 	
 	private String rgbCode;
-	private TeamEndingsMap teamEndings;
-	private TeamNicknameMap teamNicknames;
-	private PlaceInventory places;
+	private TeamEndingsMap teamEndingMap;
+	private TeamNicknameMap teamNicknameMap;
+	private PlaceInventory placeInventory;
+	private ColourTable colourTable;
+	private HashMap<String, Vector<String>> teamEndings;
+	private HashMap<String, Vector<String>> teamNicknames;
+	private Vector<String> places;
 	
 	public TeamNamer(String rgb){
 		rgbCode = rgb;
-		teamEndings = new TeamEndingsMap("TeamEndingsOutput.txt");
-		teamNicknames = new TeamNicknameMap();
-		places = new PlaceInventory();
+		teamEndingMap = new TeamEndingsMap("newTeamEndings.txt");
+		teamNicknameMap = new TeamNicknameMap();
+		placeInventory = new PlaceInventory();
+		colourTable = new ColourTable("colours.txt");
+		teamEndings = teamEndingMap.getTeamEndingMap();
+		teamNicknames = teamNicknameMap.getNicknames();
+		places = placeInventory.getPlaces();
+	}
+	
+	public String generateName(){
+		String place = placeInventory.getRandomPlace();
+		String ending = generateEnding();
+		String nickname = generateNickname(ending);
+		
+		return "The " + place + " " + ending + " aka the " + nickname;
+	}
+	
+	private String generateEnding(){
+		String ending = "ERROR";
+		double shortestDistance = 1000.0;
+		double distance;
+		
+		Iterator i = teamEndings.entrySet().iterator();
+		while(i.hasNext()){
+			Map.Entry<String, Vector<String>> pairs = (Map.Entry<String, Vector<String>>)i.next();
+			Iterator<String> j = pairs.getValue().iterator();
+			while(j.hasNext()){
+				distance = colourTable.get_distance(rgbCode, j.next());
+//				System.out.println(shortestDistance);
+				if(distance < shortestDistance){
+					shortestDistance = distance;
+					ending = pairs.getKey();
+				}
+			}
+		}
+		return ending;
+	}
+	
+	private String generateNickname(String teamEnding){
+		Vector<String> potentialNicknames = teamNicknames.get(teamEnding);
+		int rand = (int) Math.random() * potentialNicknames.size();
+		return potentialNicknames.elementAt(rand);
+	}
+	
+	public static void main(String args[]){
+		TeamNamer namer = new TeamNamer("#008000");
+		System.out.println(namer.generateName());
 	}
 }
